b39eff6228f2e933bef5c9c4c4830e69d0d5913b
Nigel Kibodeaux <nigel@team.about.me>
2017-09-10 10:05:46 -0800

    add a function. reformat code.

--- fetch.js
+++ fetch.js
@@ -11,70 +11,77 @@ var SENSOR_DATA_URL = 'https://cors-anywhere.herokuapp.com/https://bmon.analysis
 var AGE_LIMIT = 7 // record age in days
 
 
-function buildSensorUrl(sensor_name) {
-  let sd = new Date()
-  sd.setDate(sd.getDate() - 7)
-  let start_date_string = sd.toISOString().split('T')[0]
+function buildSensorUrl(sensor_name, averaging) {
+    let sd = new Date()
+    sd.setDate(sd.getDate() - 7)
+    let start_date_string = sd.toISOString().split('T')[0]
 
-  return `${SENSOR_DATA_URL}${sensor_name}?averaging=1D&start_ts=${start_date_string}`
+    return `${SENSOR_DATA_URL}${sensor_name}?averaging=${averaging}&start_ts=${start_date_string}`
 }
 
 
 // gets a list of all sensors
 async function getAllSensors() {
-  if (window.ALL_SENSORS_CACHE) {
-    return Promise.resolve(ALL_SENSORS_CACHE.data.sensors)
-  }
+    if (window.ALL_SENSORS_CACHE) {
+        return Promise.resolve(ALL_SENSORS_CACHE.data.sensors)
+    }
 
-  let response = await fetch(ALL_SENSORS_URL)
-  let json = await response.json()
-  return json.data.sensors
+    let response = await fetch(ALL_SENSORS_URL)
+    let json = await response.json()
+    return json.data.sensors
 }
 
 
 // takes the number from the gas meter
 async function getSensorNameForId(sensor_id) {
-  let sensors = await getAllSensors()
-  let sensor = sensors.find(sensor => {
-    return sensor.sensor_id.endsWith('_' + sensor_id)
-  })
-  return sensor.sensor_id
+    let sensors = await getAllSensors()
+    let sensor = sensors.find(sensor => {
+        return sensor.sensor_id.endsWith('_' + sensor_id)
+    })
+    return sensor.sensor_id
 }
 
 
 // takes the numeric id
 async function getDataForSensorId(sensor_id) {
-  let sensor_name = await getSensorNameForId(sensor_id)
-  let response = await fetch(buildSensorUrl(sensor_name))
-  let json = await response.json()
-  return json.data
+    let sensor_name = await getSensorNameForId(sensor_id)
+    let response = await fetch(buildSensorUrl(sensor_name, '1D'))
+    let json = await response.json()
+    return json.data
 }
 
 
 // takes the full name (like "joehouse_2345")
 async function getDataForSensorName(sensor_name) {
-  let response = await fetch(buildSensorUrl(sensor_name))
-  let json = await response.json()
-  return json.data
+    let response = await fetch(buildSensorUrl(sensor_name, '1D'))
+    let json = await response.json()
+    return json.data
+}
+
+// takes the full name (like "joehouse_2345")
+async function getDailyDataForSensorName(sensor_name) {
+    let response = await fetch(buildSensorUrl(sensor_name, ''))
+    let json = await response.json()
+    return json.data
 }
 
 
 // takes the number from the gas meter, returns all data for the neighborhood
 async function getDataForNeighborhood(sensor_id) {
-  let all_sensors = await getAllSensors()
-  let sensor_name = all_sensors
-    .find(sensor => {
-      console.log(sensor)
-      return sensor.sensor_id && sensor.sensor_id.endsWith('_' + sensor_id)
-    })
-    .sensor_id
+    let all_sensors = await getAllSensors()
+    let sensor_name = all_sensors
+        .find(sensor => {
+            console.log(sensor)
+            return sensor.sensor_id && sensor.sensor_id.endsWith('_' + sensor_id)
+        })
+        .sensor_id
 
-  let sensor_prefix = sensor_name.split('_' + sensor_id)[0]
+    let sensor_prefix = sensor_name.split('_' + sensor_id)[0]
 
-  let regex = new RegExp(`${sensor_prefix}_\\d\\d\\d\\d\\d\\d\\d\\d$`)
-  let neighborhood_sensors = all_sensors.filter(sensor => regex.test(sensor.sensor_id))
+    let regex = new RegExp(`${sensor_prefix}_\\d\\d\\d\\d\\d\\d\\d\\d$`)
+    let neighborhood_sensors = all_sensors.filter(sensor => regex.test(sensor.sensor_id))
 
-  let promise_array = neighborhood_sensors.map(sensor => getDataForSensorName(sensor.sensor_id))
+    let promise_array = neighborhood_sensors.map(sensor => getDataForSensorName(sensor.sensor_id))
 
-  return Promise.all(promise_array)
+    return Promise.all(promise_array)
 }
